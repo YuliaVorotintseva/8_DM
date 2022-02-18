2ed1f646b51e3e00e62b0113f53fb4731f94635e
Юлия Воротинцева <yulia.vorotintseva@gmail.com>
2022-02-18 14:39:50 +0600

    added auth component, updated dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Home from "./components/Home";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import requireAuth from "./utils/RequireAuth";
+
 import axios from "axios";
 axios.defaults.baseURL = "http://127.0.0.1:8000";
 
